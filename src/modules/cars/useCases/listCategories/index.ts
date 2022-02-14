39415e65ec067194e83b688803c3ab1d95074bf9
Eduardo Elias Alves Pereira <eduardo.eliasap@gmail.com>
2022-02-14 20:49:14 -0300

    Adding import file function using multer lib

--- src/modules/cars/useCases/listCategories/index.ts
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -3,7 +3,6 @@ import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 import { ListCategoriesController } from "./ListCategoriesController";
 
 
-
 const categoriesRepository = CategoriesRepository.getInstance();
 const listCategoriesUseCase= new ListCategoriesUseCase(categoriesRepository );
 const listCategoriesController = new ListCategoriesController(listCategoriesUseCase);
