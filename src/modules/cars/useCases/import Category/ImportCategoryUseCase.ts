c4770e92ac2854249e68d1611ac0c389e843ec32
Eduardo Elias Alves Pereira <eduardo.eliasap@gmail.com>
2022-02-16 18:25:24 -0300

    Adding find by name import category file

--- src/modules/cars/useCases/import Category/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/import Category/ImportCategoryUseCase.ts
@@ -1,5 +1,5 @@
 import {parse as csvParse} from "csv-parse";
-import fs from "fs";
+import fs from "fs"; // lib File System
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 //fs modulo nativo do node (file system) executa funções
 
@@ -7,15 +7,23 @@ interface IImportCategory{
   name:string;
   description:string;
 }
+
 class ImportCategoryUseCase {
   constructor (private categoriesRepository: ICategoriesRepository){}
 
  loadCategories(file:Express.Multer.File): Promise<IImportCategory[]>{
+   // Estou definindo que o retorno será uma Promise, por isso temos
+     // resolve para retornar em caso de sucesso e
+     // reject para retornar em caso de falha
   return new Promise ((resolve, reject)=>{
-    const stream = fs.createReadStream(file.path);
+    const stream = fs.createReadStream(file.path); // Cria um stream de leitura. Permite fazer a leitura do arquivo em partes
     const categories:IImportCategory[]=[];
 
-    const parseFile= csvParse();
+    // permite separar os registro de cada linha do arquivo buscando um delimitador (o => no caso do nosso arquivo)
+    const parseFile = csvParse({
+      delimiter: '=>',
+      trim: true
+    }); 
 
     stream.pipe (parseFile);
     //pipe pega a informação lida no stream e transfere para outro lugar pré determinado
@@ -29,6 +37,7 @@ class ImportCategoryUseCase {
       });
     })
     .on("end", ()=>{
+      fs.promises.unlink(file.path)
       resolve(categories);
     })
     .on("error", (err)=>{
@@ -40,7 +49,21 @@ class ImportCategoryUseCase {
   
   async execute (file: Express.Multer.File): Promise<void> {
     const categories= await this.loadCategories(file);
-    console.log(categories);
+
+    categories.map(async (category)=>{
+      const{name,description}= category;
+
+      const existCategory=this.categoriesRepository.findByName(name);
+
+      if (!existCategory){
+        this.categoriesRepository.create({
+          name,
+          description,
+        });
+      }
+
+    });
+   
   }
 }
 
