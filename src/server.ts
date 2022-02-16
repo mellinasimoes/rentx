d1deae5f7253e2da491a722ae5cd2681d522ca1e
Eduardo Elias Alves Pereira <eduardo.eliasap@gmail.com>
2022-02-16 18:26:45 -0300

    Adding swagger category documentation

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express';
-import {router} from "./routes"
+import swaggerUi from "swagger-ui-express"; 
+
+import {router} from "./routes";
+import swaggerFile from "./swagger.json";
+//se der erro ir no arquivo tsconfig.js e colocar resolveJsonModule:true
+
+import "./database";
 
 const app=express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve,swaggerUi.setup(swaggerFile))
+//rota que contem a documentação, chama o servidor, 
+//setup arquivo json contendo a documentação
 
 app.use (router);
 
